26012f7f15e96def1559ccfc48a2b89a8fea9742
Tyler Hutcherson <tyler.hutcherson@redis.com>
2023-04-03 04:53:19 -0400

    wip

--- app/qna/__init__.py
+++ app/qna/__init__.py
@@ -1,33 +1,10 @@
 
 import typing as t
 
-from . import db
 from . import llm
 
 
-redis_conn = db.init()
-qa_chain = llm.make_qa_chain()
-
-
-def search_semantic_redis(search_query: str, n: int) -> t.List[llm.Document]:
-    """
-    Search Redis using computed embeddings from OpenAI.
-
-    Args:
-        search_query (str): Text query to embed and use in document retrieval.
-        n (int): Number of documents to consider.
-
-    Returns:
-        list[Document]: List of relevant Docs ordered by similarity score.
-    """
-    embedding = llm.openai_embeddings.embed_query(search_query)
-    return db.search_redis(
-        redis_conn,
-        k=n,
-        query_vector=embedding,
-        return_fields=["title", "content", "tokens"]
-    )
-
+qa = llm.make_qa()
 
 def answer_question_with_context(question: str):
     """
@@ -37,9 +14,8 @@ def answer_question_with_context(question: str):
         question (str): Input question from the user.
 
     Returns:
-        input_documents (list): List of Documents used for context.
-        output_text (str): Answer from the LLM.
+        source_documents (list): List of Documents used for context.
+        result (str): Answer from the LLM.
     """
-    most_relevant_docs = search_semantic_redis(question, n=5)
-    result = qa_chain({"input_documents": most_relevant_docs, "question": question})
-    return result['input_documents'], result['output_text']
+    result = qa({"query": question})
+    return result['source_documents'], result['result']
--- app/qna/llm.py
+++ app/qna/llm.py
@@ -1,15 +1,46 @@
-from langchain.docstore.document import Document
+import os
+import pandas as pd
+
+from langchain.vectorstores.redis import Redis
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-from langchain.prompts.base import RegexParser
+from langchain.chains.qa_chain import load_qa_chain
+
+
+# Redis Env Vars
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
 
 # Init OpenAI Embeddings and LLM
 openai_embeddings = OpenAIEmbeddings()
 
-# Make QA Chain
-def make_qa_chain(chain_type: str = "stuff"):
+
+def create_vector_db() -> Redis:
+    docs = pd.read_csv("https://cdn.openai.com/API/examples/data/olympics_sections_text.csv").to_dict("records")
+    texts = [doc["content"] for doc in docs]
+    metadatas = {
+        i: {
+            "title": doc["title"],
+            "heading": doc["heading"],
+            "tokens": doc["tokens"]
+        } for i, doc in enumerate(docs)
+    }
+    # Load Redis with documents
+    vector_db = Redis.from_texts(
+        texts=texts,
+        metadatas=metadatas,
+        embeddings=openai_embeddings,
+        index_name="wiki",
+        host=REDIS_HOST,
+        port=REDIS_PORT,
+        password=REDIS_PASSWORD
+    )
+    return vector_db
+
+# Make QA
+def make_qa(chain_type: str = "stuff"):
     # Prompt Template
     prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, say that you don't know, don't try to make up an answer.
 
@@ -22,18 +53,26 @@ def make_qa_chain(chain_type: str = "stuff"):
 
     Context:
     ---------
-    {summaries}
+    {context}
     ---------
     Question: {question}
     Answer:"""
     prompt = PromptTemplate(
         template=prompt_template,
-        input_variables=["summaries", "question"]
+        input_variables=["context", "question"]
     )
+
+    # Create Redis Vector DB
+    redis = create_vector_db()
+
     # Create QA Chain
-    qa_chain = load_qa_with_sources_chain(
-        OpenAI(temperature=0.1),
-        chain_type=chain_type,
-        prompt=prompt
+    qa_chain = load_qa_chain(OpenAI(temperature=0), chain_type="stuff", prompt=prompt)
+
+    # Create QA
+    qa = VectorDBQA(
+        combine_documents_chain=qa_chain,
+        vectorstore=redis,
+        return_source_documents=True
     )
-    return qa_chain
+
+    return qa
